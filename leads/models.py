0f002f9d071f4d9417df0771260abb322b5e36e9
Archit Gupta <rarchitgupta@gmail.com>
2022-04-08 10:27:01 +0530

    New User model and migrations

--- leads/models.py
+++ leads/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
 class Lead(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
